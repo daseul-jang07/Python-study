3c35b4838aba74966e06ca684276b961af059b70
daseul-jang07 <daseuljang24@gmail.com>
2025-10-01 18:22:10 +0900

    first commit

---
+++ study/(2-4) 변수.py
@@ -0,0 +1,18 @@
+# 애완동물을 소개해 주세요 ~
+animal = "강아지"
+name = "연탄이"
+age = 4
+hobby = "산책"
+is_adult = age >= 3
+
+print("우리집 " + animal + "의 이름은 " + name + "에요")
+hobby = "공놀이"
+print(name + "는 " + str(age) + "살이며, " + hobby + "을 아주 좋아해요")
+print(name + "는 어른일까요? " + str(is_adult))
+
+# +를 ,으로 대체 가능 -> 정수형 변수나 boolean 변수를 str 없이 사용 가능, 띄어쓰기 포함
+'''작은 따옴표 양쪽에
+3개씩 달아주면
+여러 문장
+주석 처리
+가능'''
