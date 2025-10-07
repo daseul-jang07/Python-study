40de143de8f95ed5353297e400ada60994f07546
daseul-jang07 <daseuljang24@gmail.com>
2025-10-07 20:27:42 +0900

    update

--- study/(5-4) 세트.py
+++ study/(5-4) 세트.py
@@ -14,3 +14,7 @@ print(java.intersection(python))
 # 합집합 (java 할 수 있거나 python 할 수 있는 개발자)
 print(java | python)
 print(java.union(python))
+
+# 차집합 (java는 할 수 있지만 python은 할 줄 모르는 개발자)
+print(java - python)
+print(java.difference(python))
